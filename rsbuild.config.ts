80458865f724f69a29211759840ed61a5817cadd
ntbloom <ntbloom@gmail.com>
2025-08-11 22:46:59 -0400

    add react compiler
    * remove react-router
    * rearrange dependencies

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from '@rsbuild/core';
+import { pluginBabel } from '@rsbuild/plugin-babel';
 import { pluginReact } from '@rsbuild/plugin-react';
 
+const ReactCompilerConfig = {};
+
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [
+    pluginReact(),
+    pluginBabel({
+      include: /\.(?:jsx|tsx)$/,
+      babelLoaderOptions(opts) {
+        opts.plugins?.unshift(
+          'babel-plugin-react-compiler',
+          ReactCompilerConfig,
+        );
+      },
+    }),
+  ],
   html: {
     template: './public/index.html',
   },
